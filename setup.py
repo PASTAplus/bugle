eeda9a3c161cd3ea3d8f85a96a360c845f344648
Mark Servilla <mark.servilla@gmail.com>
2022-02-06 09:15:36 -0700

    Setup project framework

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: setup.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    6/15/18
+"""
+from os import path
+from setuptools import find_packages, setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(path.join(here, "LICENSE"), encoding="utf-8") as f:
+    full_license = f.read()
+
+with open(path.join(here, "src/bugle/VERSION.txt"), encoding="utf-8") as f:
+    version = f.read()
+
+
+setup(
+    name="bugle",
+    version=version,
+    description="Bugle: a light-weight web site crawler adn search indexer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Environmental Data Initiative",
+    url="https://github.com/PASTAplus/bugle",
+    license=full_license,
+    packages=find_packages(where="src", include=["bugle",]),
+    package_dir={"": "src"},
+    include_package_data=True,
+    python_requires=" == 3.10.*",
+    install_requires=["click==8.0.3", "daiquiri==3.0.0",  "scrapy==2.4.1",],
+    classifiers=["License :: OSI Approved :: Apache Software License",],
+)
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
