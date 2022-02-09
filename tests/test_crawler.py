5c247852e165387ae2ea0de9854a87f023d8f812
Mark Servilla <mark.servilla@gmail.com>
2022-02-08 21:13:56 -0700

    Comply with Crawler class crawl signature

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -14,19 +14,26 @@
 """
 import pytest
 
+from bs4 import BeautifulSoup
 from bugle.scrape.config import Config
 from bugle.scrape.crawler import Crawler
 from bugle.scrape.page import Page
 
 
 def test_crawl():
-    crawler = Crawler(host="https://web-x.edirepository.org", path=Config.PATH, callback=page_bugle)
-    crawler.crawl()
+    print("\n")
+    crawler = Crawler(host="http://localhost:8000", path=Config.PATH)
+    crawler.crawl(allow="^/", callback=page_bugle)
     print(crawler.visited)
 
 
 def page_bugle(**kwargs):
     page: Page = kwargs["page"]
-    print(page.url)
-    path: str = kwargs["path"]
-    print(path)
+    # print(page.url)
+    visited: list = kwargs["visited"]
+    # print(visited)
+    soup = BeautifulSoup(page.html, "lxml")
+    main_tag = soup.main
+    if main_tag is not None:
+        index_text = " ".join([_.strip() for _ in main_tag.strings])
+        print(f"{page.url}: {index_text}")
