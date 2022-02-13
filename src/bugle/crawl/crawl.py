792d9343ea0939148244dbe44a54d6d43b1d2288
Mark Servilla <mark.servilla@gmail.com>
2022-02-12 19:49:41 -0700

    Refactor logic; write out index content to cache location

--- src/bugle/crawl/crawl.py
+++ src/bugle/crawl/crawl.py
@@ -12,50 +12,48 @@
 :Created:
     2/9/22
 """
-from bs4 import BeautifulSoup
+import json
+import logging
+import os
+
 import click
 import daiquiri
 
+from bugle.config import Config
 from bugle.crawl.crawler import Crawler
-from bugle.crawl.page import Page
 
+cwd = os.path.dirname(os.path.realpath(__file__))
+logfile = cwd + "/crawl.log"
+daiquiri.setup(level=logging.INFO,
+               outputs=(daiquiri.output.File(logfile), "stdout",))
 logger = daiquiri.getLogger(__name__)
 
 
-def indexer(**kwargs):
-    page: Page = kwargs["page"]
-    soup = BeautifulSoup(page.html, "lxml")
-    main_tag = soup.main
-    if main_tag is not None:
-        index_text = " ".join([_.strip() for _ in main_tag.strings])
-        print(f"{page.url}: {index_text}")
-
-
-allow_help = "Filter HREFs using regular expression - default: '*' (allow all HREFs)"
+allow_help = "Allow HREFs using regular expression - default: '*' (allow all HREFs)"
 follow_help = "Follow HREFs on page to next page(s)"
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.argument("host", nargs=1, required=True)
-@click.argument("paths", nargs=-1, required=True)
+@click.argument("url", nargs=1, required=True)
+@click.argument("selectors", nargs=-1, required=True)
 @click.option("-a", "--allow", default="*", help=allow_help)
 @click.option("-f", "--follow", is_flag=True, default=False, help=follow_help)
-def main(host: str, paths: tuple, allow: str, follow: bool):
+def main(url: str, selectors: tuple, allow: str, follow: bool):
     """
         Crawl a website
 
         \b
-            HOST: Website host name (e.g., https://web.myweb.me).
-            PATHS: One or more space separated page paths.
+            URL: URL of website to crawl and index
+            SELECTORS: One or more space separated tags to define index content.
     """
+    crawler = Crawler(url=url)
+    crawler.crawl(selectors=selectors, allow=allow, follow=follow)
+    logger.info(crawler.visited)
 
-    crawler = Crawler(host=host)
-
-    for path in paths:
-        crawler.crawl(path=path, allow=allow, follow=follow, callback=indexer)
-
-    print(crawler.visited)
+    j = json.dumps(crawler.index_content, indent=2)
+    with open(Config.INDEX_CONTENT_CACHE , "w") as f:
+        f.write(j)
 
     return 0
 
