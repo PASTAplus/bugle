b93483c8bb0718460af4e7115c24bb0b937fe93f
Mark Servilla <mark.servilla@gmail.com>
2022-02-15 17:23:49 -0700

    Support reading pickled index

--- src/bugle/index/load.py
+++ src/bugle/index/load.py
@@ -13,6 +13,7 @@
     2/12/22
 """
 import json
+import pickle
 
 import daiquiri
 
@@ -23,13 +24,21 @@ from bugle.index.webpage import WebPage
 logger = daiquiri.getLogger(__name__)
 
 
-def load_content(path: str) -> Index:
-    with open(path, "r") as f:
-        content = json.loads(f.read())
+def load_content(content_path: str) -> dict:
+    with open(content_path, "r") as f:
+        return json.loads(f.read())
 
+
+def build_index(content: dict) -> Index:
     index = Index()
     for i, page_url in enumerate(content):
         webpage = WebPage(ID=i, content=content[page_url], url=page_url)
         index.index_document(webpage)
-
     return index
+
+
+def from_pkl(index_path: str) -> Index:
+    with open(index_path, "rb") as f:
+        index = pickle.load(f)
+    return index
+
