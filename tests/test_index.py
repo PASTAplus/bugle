054500f4d94b4ba58fe37eb43cdc5e9133029917
Mark Servilla <mark.servilla@gmail.com>
2022-02-15 17:24:24 -0700

    Test loading pickled index

--- tests/test_index.py
+++ tests/test_index.py
@@ -12,6 +12,7 @@
 :Created:
     2/12/22
 """
+import datetime
 import json
 
 import daiquiri
@@ -25,19 +26,36 @@ logger = daiquiri.getLogger(__name__)
 
 
 def test_load_content():
-    with open(Config.INDEX_CONTENT_CACHE, "r") as f:
+    with open(f"{Config.CACHE}/content.json", "r") as f:
         content = json.loads(f.read())
-
     assert isinstance(content, dict)
 
 
 def test_index_webpage():
-    index: Index = load.load_content(Config.INDEX_CONTENT_CACHE)
+    content = load.load_content(f"{Config.CACHE}/content.json")
+    index: Index = load.build_index(content)
     assert isinstance(index, Index)
 
 
 def test_search_index():
-    index: Index = load.load_content(Config.INDEX_CONTENT_CACHE)
+    content = load.load_content(f"{Config.CACHE}/content.json")
+    index: Index = load.build_index(content)
+    hits = index.search("Environmental")
+    assert len(hits) > 0
+    hits = index.search("Etiquette")
+    assert len(hits) >= 2
+    hits = index.search("Bullwinkle")
+    assert len(hits) == 0
+    hits = index.search("Etiquette", rank=True)
+    assert len(hits) >= 2
+    hits = index.search("Etiquette Environmental", search_type="AND", rank=True)
+    assert len(hits) == 1
+    hits = index.search("Etiquette Environmental", search_type="OR", rank=True)
+    assert len(hits) == 4
+
+
+def test_pkl_search_index():
+    index: Index = load.from_pkl(f"{Config.CACHE}/index.pkl")
     hits = index.search("Environmental")
     assert len(hits) > 0
     hits = index.search("Etiquette")
